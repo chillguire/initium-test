a999bbb84ceb62af28e0d39a22b42f127407f1e9
Ricardo Avendaño <ricardo.avendano@sybven.com>
2021-07-11 11:23:02 -0400

    initial commit. setting things up

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const path = require('path');
+
+const mongoose = require('mongoose');
+
+
+//** DB CONFIG
+const dbURL = process.env.DB_URL || 'mongodb://127.0.0.1:27017/customerSupport';
+mongoose.connect(dbURL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+	console.log('DB connected');
+});
+
+//** APP CONFIG
+//? general
+app.use(express.urlencoded({ extended: true, }));
+// app.use(express.json());
+
+//? views and static files
+app.set('view engine', 'ejs');
+app.set('view', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//** ROUTES
+app.get('/', (req, res) => {
+	res.send('hola');
+});
+
+app.all('*', (req, res, next) => {
+	res.sendStatus(404);
+});
+
+//** APP.LISTEN
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+	console.log(`Running: ${port}`);
+});
